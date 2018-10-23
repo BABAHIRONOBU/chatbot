3c1c29997f16d99285b074eabf696716ac1a689b
BABA HIRONOBU <ghdtjs0417@gmail.com>
2018-10-23 10:44:41 +0900

    Update brs_chatbot.js

--- brs_chatbot.js
+++ brs_chatbot.js
@@ -1,10 +1,9 @@
 console.log('안녕하세요!! 브루스 챗봇입니다!!!!!!!');
-window.onload = function () {
-  var div = document.createElement('div');
-  div.setAttribute('id', 'app');
-  document.getElementsByTagName('body')[0].appendChild(div);
 
-  var script = document.createElement('script');
-  script.setAttribute('src', './dist/build.js');
-  document.getElementsByTagName('body')[0].appendChild(script);
-}
+var div = document.createElement('div');
+div.setAttribute('id', 'app');
+document.getElementsByTagName('body')[0].appendChild(div);
+
+var script = document.createElement('script');
+script.setAttribute('src', 'https://babahironobu.github.io/chatbot/dist/build.js');
+document.getElementsByTagName('body')[0].appendChild(script);
